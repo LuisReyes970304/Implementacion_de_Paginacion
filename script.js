c7d8d5c2652533ca3fb671874b38bb3651002024
dilandcv <josemiguelvargas_@hotmail.com>
2026-05-06 06:55:55 -0500

    firts commit

--- script.js
+++ script.js
@@ -1,77 +1,57 @@
-
-// const body = document.querySelector("body");
-
-
-
-// async function callCharacter(url) {
-//     const response = await fetch(url_api);
-//     const data = await response.json();
-//     console.log(data);
-    
-//     data.results.forEach(personaje => {
-//         const newElement = document.createElement("div");
-//         newElement.innerHTML = `<img src="${personaje.image}"> <p>${personaje.name}</p>`;
-//         body.appendChild(newElement);
-//     })
-// }
-
-// callCharacter(url_api);
-
-// async function callCharacter(url_api) {
-//     const response = await fetch(url_api);
-//     let data = await response.json();
-//     console.log(data.results);
-
-//     let index = 0;
-//     addCharacter.addEventListener("click", () => {
-//         let name = data.results[index].name;
-//         let gender = data.results[index].gender;
-//         let image = data.results[index].image;
-//         unList.innerHTML += `<li>
-//         <img src="${image}">
-//         <h2>${name}</h2>
-//         <p>${gender}<p>
-//         </li>`;
-//         index +=1;
-//     });
-// };
-
+// URL endpoint
 const url_api = "https://rickandmortyapi.com/api/character";
-
-
-callCharacter(url_api);
-
-async function callCharacter(url_api) {
-    const addnext = document.getElementById("loadMore");
-    const response = await fetch(url_api);
-    const unList = document.querySelector(".unList");
-    let data = await response.json();
-    let info = data.info;
-    addnext.setAttribute("data-next", info.next) ? "": info.next;
-    addnext.setAttribute("data-prev", info.prev) ? "": info.prev;
-    console.log(data.results);
-
-    for (let index=0; index < data.results.length; index++) {
-        let name = data.results[index].name;
-        let gender = data.results[index].gender;
-        let image = data.results[index].image;
-        let status = data.results[index].status;
-        console.log(name);
-        unList.innerHTML += `<li>
-        <img src="${image}">
-        <h2>${name}</h2>
-        <p>${gender}<p>
-        </li>`;
-    };
-};
-
-function loadMore() {
-    const addnext = document.getElementById("loadMore");
-    const next = addnext.getAttribute("data-next");
-    if(next == "" || next == null){
-        console.log("There is not URL");
-    } else {
-        callCharacter(next);
-    }
+/**
+requestData
+Send request to Endpoint
+@param {string} url_api**/
+async function requestData(url_api) {
+ const response = await fetch(url_api);
+ let data = await response.json();
+ getElementButton(document, 'set', data.info)
+ renderHtml(data);
 }
-
+/**
+loadMore
+Call @Function getElementButton */
+function loadMore() {
+ getElementButton(document, 'get')
+}
+/**
+getElementButton
+@param {object} elementButton
+@param {object} button
+@param {string} operation*/
+function getElementButton(elementButton, operation = 'get', info = null)
+{
+ const button = elementButton.getElementById("loadMore");
+ if(operation == 'get'){
+ const next = button.getAttribute("data-next");
+ if(next == "" || next == null){
+ console.log("No hay url")
+ }else {
+ requestData(next);
+ }
+ }else {
+ button.setAttribute("data-next", (info.next == null) ? '' :
+info.next)
+ button.setAttribute("data-prev", (info.prev == null) ? '' :
+info.prev)
+ }
+}
+/**
+renderHtml
+@param {object} element
+@param {object} data*/
+function renderHtml(data){
+ let element = document.getElementById("character");
+ let resultCount = data.results.length;
+ for (let index = 0; index < resultCount; index++) {
+ let character = data.results[index];
+ element.innerHTML += `<li>
+ <img src="${character.image}" alt="${character.name}">
+ <h2>${character.name}</h2>
+ <span>${character.gender}</span>
+ </li>`;
+ }
+}
+const response = requestData(url_api);
