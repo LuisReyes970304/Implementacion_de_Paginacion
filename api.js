c2cd9fd5191c88b05c3c1eae33a0c00504f48a16
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-05-06 08:55:31 -0500

    button modified

--- api.js
+++ api.js
@@ -1,55 +1,61 @@
 // URL endpoint
 const url_api = "https://rickandmortyapi.com/api/character";
 /**
-requestData
-Send request to Endpoint
-@param {string} url_api**/
+*requestData
+*Send request to Endpoint
+*@param {string} url_api
+*/
 async function requestData(url_api) {
- const response = await axios.get(url_api);
- let data = response.data;
- getElementButton(document, 'set', data.info)
- renderHtml(data);
+    const response = await axios.get(url_api);
+    let data = response.data;
+    console.log(data)
+    getElementButton(document, 'set', data.info);
+    renderHtml(data);
 }
+
 /**
-loadMore
-Call @Function getElementButton */
+*loadMore
+*Call @Function getElementButton */
 function loadMore() {
- getElementButton(document, 'get')
+    getElementButton(document, 'get');
 }
+
 /**
-getElementButton
-@param {object} elementButton
-@param {object} button
-@param {string} operation*/
+*getElementButton
+*@param {object} elementButton
+*@param {object} button
+*@param {string} operation
+*/
 function getElementButton(elementButton, operation = 'get', info = null)
 {
- const button = elementButton.getElementById("loadMore");
- if(operation == 'get'){
- const next = button.getAttribute("data-next");
-    if(next == "" || next == null){
-        console.log("No hay url")
+    const button = elementButton.getElementById("loadMore");
+    if(operation == 'get'){
+    const next = button.getAttribute("data-next");
+        if(next == "" || next == null){
+            console.log("No hay url")
+        }else {
+            requestData(next);
+        }
     }else {
-        requestData(next);
+        button.setAttribute("data-next", (info.next == null) ? '' : info.next)
+        button.setAttribute("data-prev", (info.prev == null) ? '' : info.prev)
     }
- }else {
-    button.setAttribute("data-next", (info.next == null) ? '' : info.next)
-    button.setAttribute("data-prev", (info.prev == null) ? '' : info.prev)
- }
 }
 /**
-renderHtml
-@param {object} element
-@param {object} data*/
+*renderHtml
+*@param {object} element
+*@param {object} data
+*/
 function renderHtml(data){
- let element = document.getElementById("character");
- let resultCount = data.results.length;
- for (let index = 0; index < resultCount; index++) {
- let character = data.results[index];
- element.innerHTML += `<li>
- <img src="${character.image}" alt="${character.name}">
- <h2>${character.name}</h2>
- <span>${character.gender}</span>
- </li>`;
- }
+    let element = document.getElementById("character");
+    let resultCount = data.results.length;
+    for (let index = 0; index < resultCount; index++) {
+    let character = data.results[index];
+    element.innerHTML = `<li>
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
+        <span>${character.gender}</span>
+        </li>`;
+    }
 }
 const response = requestData(url_api);
